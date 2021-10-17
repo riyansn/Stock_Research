fc73e1e861fd414670605e1f63b945d679d241b1
riyansn <ryansthefanus@gmail.com>
2021-10-17 21:41:37 +0700

    optimize getStockCode.py

--- getStockCode.py
+++ getStockCode.py
@@ -1,24 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
 driver= webdriver.Chrome("C:/WebDriver/bin/chromedriver.exe")
 driver.get("https://www.idx.co.id/perusahaan-tercatat/profil-perusahaan-tercatat/")
-
 Select(driver.find_element(By.XPATH,"//select[@name='companyTable_length']")).select_by_value('100')
-time.sleep(1.2)
 f=open("C:/Users/Astrowest/Documents/Persahaman/KodeEmiten.txt",'w+')
-
+WebDriverWait(driver,10).until(lambda driver: len(driver.find_elements(By.XPATH,"//tbody/tr[@role='row']/td[2]")) == 100)
 for n in range(8):
     if(n>0):
         driver.find_element(By.XPATH,"//a[@class='paginate_button next']").send_keys(Keys.ENTER)
-        time.sleep(1)
+        WebDriverWait(driver,10).until(lambda driver : int(driver.find_element(By.XPATH,"//tbody/tr[@role='row'][1]/td[1]").text ) > before)
+    before = int(driver.find_element(By.XPATH,"//tbody/tr[@role='row'][1]/td[1]").text)
     for emitenCode in driver.find_elements(By.XPATH,"//tbody/tr[@role='row']/td[2]"):
         print(emitenCode.text)
-        time.sleep(0.3)
         f.write(emitenCode.text+"\n")
+        time.sleep(0.05)
+    
 f.close()
